e9c4186802c8abc1186f40df005d75d35d60449e
Ataides-Blume <abo.revelacao@outlook.com>
2025-09-24 19:43:58 -0300

    Ajuste no vite.config.js para rodar pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/projeto-pokequest-react-avancado/",
   plugins: [react()],
+  base: "/projeto-pokequest-react-avancado/",
 });
