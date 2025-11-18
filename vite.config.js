e0dd7b5c5237778ba9c4aa40d013dd6b3a555d9b
Ataides-Blume <abo.revelacao@outlook.com>
2025-11-18 18:22:40 -0300

    removido o base do vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/projeto-pokequest-react-avancado/",
+  // base: "/projeto-pokequest-react-avancado/",
 });
